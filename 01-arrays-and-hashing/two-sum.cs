fb8faaffa1468ee3a5038f651491a2e5aa8b3d35
brian054 <brian.rowell54@gmail.com>
2026-08-18 13:10:23 +0100

    two sum C#

--- 01-arrays-and-hashing/two-sum.cs
+++ 01-arrays-and-hashing/two-sum.cs
@@ -1,7 +1,33 @@
 ﻿// Problem: Two Sum
 // Category: Arrays and Hashing
 
-public class Solution
-{
-    
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> HashMap = new Dictionary<int, int>();
+        // First solution off the dome
+        // Build HashMap
+        // for(int i = 0; i < nums.Length; i++) {
+        //     HashMap[nums[i]] = i;
+        // }
+        // // Check for target match
+        // for(int i = 0; i < nums.Length; i++) {
+        //     int potentialCorrectKey = target - nums[i];
+        //     if (HashMap.ContainsKey(potentialCorrectKey) && HashMap[potentialCorrectKey] != i) {
+        //         return [i, HashMap[potentialCorrectKey]];
+        //     }
+        // }
+
+        // This much better
+        for (int i = 0; i < nums.Length; i++) {
+            int potentialKeyNeeded = target - nums[i];
+
+            if (HashMap.ContainsKey(potentialKeyNeeded)) {
+                return [i, HashMap[potentialKeyNeeded]];
+            }
+
+            HashMap[nums[i]] = i;
+        }
+        // We're guarenteed an answer so this should not occur.
+        return [-1, -1];
+    }
 }
