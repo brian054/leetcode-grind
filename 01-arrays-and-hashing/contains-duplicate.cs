7d53cbbd46039ca02cf2353131c0f73c6b0b8e3d
brian054 <brian.rowell54@gmail.com>
2026-08-19 13:19:40 +0100

    contains duplicate

--- 01-arrays-and-hashing/contains-duplicate.cs
+++ 01-arrays-and-hashing/contains-duplicate.cs
@@ -1,7 +1,18 @@
 ﻿// Problem: Contains Duplicate
 // Category: Arrays and Hashing
 
-public class Solution
-{
-    
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        Dictionary<int, int> HashMap = new();
+
+        foreach(var number in nums) {
+            // add to HM
+            if (HashMap.ContainsKey(number)) {
+                return true;
+            } else {
+                HashMap.Add(number, 1); // key = nums[i], value = number of occurences
+            }
+        }
+        return false; 
+    }
 }
