ae42a9a8fc50c268f59a2fcd90964a18ebebf246
brian054 <brian.rowell54@gmail.com>
2026-08-20 17:05:42 +0100

    valid anagram

--- 01-arrays-and-hashing/valid-anagram.cs
+++ 01-arrays-and-hashing/valid-anagram.cs
@@ -1,7 +1,34 @@
 ﻿// Problem: Valid Anagram
 // Category: Arrays and Hashing
 
-public class Solution
-{
-    
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length) {
+            return false;
+        }
+
+        Dictionary<char, int> hm = new();
+
+        // build hm
+        foreach (var letter in s) {
+            if (hm.ContainsKey(letter)) {
+                hm[letter] += 1;
+            } else {
+                hm[letter] = 1;
+            }
+        }
+
+        // see if match or not
+        foreach (var letter in t) {
+            if(hm.ContainsKey(letter)) {
+                hm[letter] -= 1;
+                if (hm[letter] < 0) {
+                    return false;
+                } 
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 }
