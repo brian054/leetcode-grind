6adb7bb98342d47df910f703f3eb7a02892fac10
brian054 <brian.rowell54@gmail.com>
2026-08-21 17:38:34 +0100

    group anagram

--- 01-arrays-and-hashing/group-anagrams.cs
+++ 01-arrays-and-hashing/group-anagrams.cs
@@ -1,7 +1,29 @@
 ﻿// Problem: Group Anagrams
 // Category: Arrays and Hashing
 
-public class Solution
-{
-    
+public class Solution {
+    public IList<IList<string>> GroupAnagrams(string[] strs) {
+        if (strs.Length == 1) {
+            return new List<IList<string>> {
+                new List<string> { strs[0] }
+            };
+        } 
+
+        Dictionary<string, IList<string>> hm = new();
+        for(int i = 0; i < strs.Length; i++) {
+            string alphabeticalOrderString = new string(strs[i].OrderBy(c => c).ToArray());
+
+            if (hm.ContainsKey(alphabeticalOrderString)) {
+                // add onto the values List
+                hm[alphabeticalOrderString].Add(strs[i]);
+            } else {
+                // create new hashmap element
+                hm.Add(alphabeticalOrderString, new List<string> {strs[i]});
+            }
+        }
+
+        // Build Output
+        IList<IList<string>> output = hm.Values.ToList();
+        return output;
+    }
 }
---
+++ Playground/Program.cs
@@ -0,0 +1,19 @@
+﻿// string word = "tea";
+
+// var ordered = word.OrderBy(c => c);
+
+// Console.WriteLine(string.Join("", ordered));
+
+Dictionary<string, List<string>> hm = new();
+
+hm["aet"] = new List<string> { "eat", "tea", "ate" };
+hm["ant"] = new List<string> { "tan", "nat" };
+hm["abt"] = new List<string> { "bat" };
+
+List<List<string>> output = hm.Values.ToList().ToString();
+Console.WriteLine(string.Join(", ", output));
+
+foreach (var group in hm.Values)
+{
+    Console.WriteLine(string.Join(", ", group));
+}
